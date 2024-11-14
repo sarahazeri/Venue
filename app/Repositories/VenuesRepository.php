7bcd6ae6c31018efd80edf215ee9bc73551821a9
Hazeri <hazeri.sara@gmail.com>
2024-11-15 03:49:46 +0400

    Cashing filters

--- app/Repositories/VenuesRepository.php
+++ app/Repositories/VenuesRepository.php
@@ -5,6 +5,7 @@ namespace App\Repositories;
 
 use App\Models\Venue;
 use App\Models\Weight;
+use Illuminate\Support\Facades\Cache;
 
 class VenuesRepository
 {
@@ -16,27 +17,20 @@ class VenuesRepository
 
     public function getVenuesByEventType($category, $categoryId)
     {
-        // Fetch venues based on a specific event type with custom sorting by weight
-        // دریافت وزن‌ها برای دسته‌بندی انتخاب‌شده
-        $weights = Weight::where(['category_type' => $category, 'category_id' => $categoryId])
-            ->get();
-        // استخراج venue_ids از وزن‌ها
-        $venueIds = $weights->pluck('venues_id'); // اینجا فرض می‌کنیم که فیلد venue_id در جدول weights است
-
-        // دریافت venue ها با استفاده از venue_ids
-        $venues = Venue::whereIn('id', $venueIds)->get();
-
-        // حالا می‌توانید اقدام به مرتب‌سازی یا هر عملی روی نتایج کنید
-        $venues = $venues->map(function ($venue) use ($weights) {
-            // پیدا کردن وزن مرتبط با هر venue
-            $venueWeight = $weights->firstWhere('venues_id', $venue->id);
-
-            // اگر وزنی برای venue پیدا نشد، مقدار پیش‌فرض صفر قرار می‌دهیم
-            $venue->weight = $venueWeight ? $venueWeight->weight : 0;
-
-            return $venue;
+        $venues = Cache::remember('venues_by_category_' . $category . '_' . $categoryId, now()->addMinutes(10), function () use ($category, $categoryId) {
+            return Venue::join('weights', function ($join) use ($category, $categoryId) {
+                $join->on('venues.id', '=', 'weights.venues_id')
+                    ->where('weights.category_type', '=', $category)
+                    ->where('weights.category_id', '=', $categoryId);
+            })
+                ->select('venues.*', 'weights.weight')
+                ->orderByDesc('weights.weight')
+                ->get();
         });
 
-        return $venues->sortBy('weight');
+
+        return $venues;
     }
+
+
 }
